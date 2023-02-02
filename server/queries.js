39ae7b5cdc78dfd6a8761c23245174b8893d32dd
Vatsal <vatsalgoyal44@gmail.com>
2023-02-02 18:57:22 +0530

    login page and database connection

--- server/queries.js
+++ server/queries.js
@@ -43,3 +43,30 @@ const courselistall = () => {
         return results.rows
     })
 }
+
+const studentinfo = async (ID) => {
+
+    const text = 'SELECT * FROM student WHERE id = $1'
+    const values = [ID]
+
+    try {
+        const res = await pool.query(text, values)
+        console.log(res.rows[0])
+        return res.rows[0];
+        const text = 'SELECT * FROM takes WHERE id = $1'
+        const values = [ID]
+      } catch (err) {
+        console.log(err.stack)
+      }
+      
+
+    // pool.query('SELECT * FROM student WHERE id = 1', (error, results) => {
+    //     if (error) {
+    //         throw error
+    //     }
+    //     console.log(results.rows)
+    //     return results.rows[0]
+    // })
+}
+
+module.exports.studentinfo = studentinfo;
